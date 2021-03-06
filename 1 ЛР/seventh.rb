a951956f01eff98a4caaf5326c3e2a0e754a1df0
Nat-chan256 <Nat.Chan.Des@yandex.ru>
2021-03-06 15:52:15 +0300

    1 ЛР. 8 задание. 2 метод

--- 1 ЛР/seventh.rb
+++ 1 ЛР/seventh.rb
@@ -9,6 +9,12 @@ def isPrime(num)
 	result
 end
 
+def digitsNotDivisibleBy5Product(num)
+	prod = 1
+	num.digits.map{ |digit| prod *= digit if digit % 5 != 0 }
+	prod
+end
+
 def maxPrimeDelimeter(num)
 	maxDel = num
 	for i in (1..num).to_a.reverse do
