8c5162bc544048601edfa3a41e175d297c38c7ae
Nat-chan256 <Nat.Chan.Des@yandex.ru>
2021-03-06 15:53:06 +0300

    1 ЛР. 7 задание. 3 метод

--- 1 ЛР/seventh.rb
+++ 1 ЛР/seventh.rb
@@ -25,3 +25,39 @@ def maxPrimeDelimeter(num)
 	end
 	maxDel
 end
+
+def GCF(num1, num2)
+	while num1 != num2
+		if num1 > num2
+			num1 -= num2
+		else
+			num2 -= num1
+		end
+	end
+	
+	num1
+end
+
+
+def digitsProduct(num)
+	prod = 1
+	num.digits.map{ |digit| prod *= digit if digit != 0}
+	prod
+end
+
+def maxOddNotPrimeDelimeter(num)
+	del = 1
+	for i in (1..num).to_a.reverse do
+		if i.odd? and not isPrime(i) and num % i == 0
+			del = i
+			break
+		end
+	end
+	del
+end
+
+def maxOddNotPrimeDelAndDigitsProdGCF(num)
+	maxDel = maxOddNotPrimeDelimeter(num)
+	digProd = digitsProduct(num)
+	gcf = GCF(maxDel, digProd)
+end
