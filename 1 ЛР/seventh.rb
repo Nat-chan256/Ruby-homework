4e74c9255db2edcf1c3d7b37f2da6b4b6f6a81d1
Nat-chan256 <Nat.Chan.Des@yandex.ru>
2021-03-06 15:50:39 +0300

    1 ЛР. 7 задание. 1 метод

---
+++ 1 ЛР/seventh.rb
@@ -0,0 +1,21 @@
+def isPrime(num)
+	result = true
+	for i in (2..(num/2)) do
+		if num % i == 0
+			result = false
+		end
+	end
+	
+	result
+end
+
+def maxPrimeDelimeter(num)
+	maxDel = num
+	for i in (1..num).to_a.reverse do
+		if num % i == 0 and isPrime(i)
+			maxDel = i
+			break
+		end
+	end
+	maxDel
+end
