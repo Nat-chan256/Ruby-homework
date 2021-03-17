88f0aff7bb367b132e85ab7c348da5e911575194
Nat-chan256 <Nat.Chan.Des@yandex.ru>
2021-03-17 19:12:09 +0300

    2 ЛР. 9 задание

--- 2 ЛР/9task.rb
+++ 2 ЛР/9task.rb
@@ -5,9 +5,9 @@ def shuffleWords(str)
 end
 
 #8
-#Количество слов в строке, состоящиз из четного количества символов
+#Количество слов в строке, состоящих из четного количества символов
 def countWordsOfEvenCharsNum(str)
-	return str.split().select{|word| word.length % 2 == 0}.count
+	return str.split().select{|word| word.length.even?}.count
 end
 
 #16
@@ -15,4 +15,42 @@ end
 def sortTricolor(arr)
 	sampleArr = ["белый", "синий", "красный"]
 	return arr.sort_by{|element| sampleArr.index(element.downcase)}
+end
+
+
+#Чтение строки из файла
+def readFromFile(filePath)
+	if not File.file?(filePath) #Проверка существования файла
+		return nil
+	end
+    file = File.new(filePath, "r:UTF-8")
+    lines = file.readlines
+	return lines.join(" ")
+end
+
+answer = "1"
+while answer != "0"
+	puts "Введите номер задачи из списка:
+	0 - выход из программы
+	1 - перемешивание слов в строке
+	2 - подсчет количества слов, состоящих из четного количеств символов
+	3 - сортировка массива, состоящего из слов 'белый', 'красный' и 'синий' в порядке цветов российского флага"
+	answer = STDIN.gets.chomp.strip
+	case answer
+		when "1"
+			print "Введите исходную строку: "
+			str = STDIN.gets.chomp
+			puts "Результат перемешивания: #{shuffleWords(str)}"
+			
+		when "2"
+			print "Введите исходную строку: "
+			str = STDIN.gets.chomp
+			puts "Количество слов, состоящих из четного числа символов: #{countWordsOfEvenCharsNum(str)}"
+			
+		when "3"
+			print "Введите путь к файлу с массивом: "
+			filePath = STDIN.gets.chomp
+			puts "Результат сортировки #{sortTricolor(readFromFile(filePath).split())}"
+		
+	end
 end
