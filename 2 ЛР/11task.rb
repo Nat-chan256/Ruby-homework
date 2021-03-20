c48d708a22839939952fe26acb3fb4c360de4f85
Nat-chan256 <Nat.Chan.Des@yandex.ru>
2021-03-20 13:38:44 +0300

    2 ЛР. 11 задание. 2 задача

--- 2 ЛР/11task.rb
+++ 2 ЛР/11task.rb
@@ -1,7 +1,13 @@
-#8, 16
+# 16
 
 #3
 #Нахождение количества русских символов
 def countCyrillics(str)
 	return str.scan(/[а-яА-Я]/).count
+end
+
+#8
+#Нахождение всех используемых строчных символов латиницы
+def findLowercaseLatin(str)
+	return str.scan(/[a-z]/).uniq
 end
