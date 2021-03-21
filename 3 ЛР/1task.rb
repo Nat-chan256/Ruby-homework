2e2ea0364197c361fd3920af9a41369414fa8a4b
Nat-chan256 <Nat.Chan.Des@yandex.ru>
2021-03-21 20:13:22 +0300

    3 ЛР. 1 задание

---
+++ 3 ЛР/1task.rb
@@ -0,0 +1,45 @@
+class Employee
+
+    attr_accessor :surname, :name, :patronymic,
+        :birthdate, :phoneNumber, :address, :email, :passSeriesNumber, :speciality,
+        :experience
+		
+
+    def initialize(*args)
+        if args.length == 9
+			@surname = args[0]
+            @name = args[1]
+            @patronymic = args[2]
+            @birthdate = args[3]
+            @phoneNumber = args[4]
+			@address = args[5]
+            @email = args[6]
+            @passSeriesNumber = args[7]
+            @speciality = args[8]
+            @experience = 0
+
+        elsif args.length == 13
+            experience = args[9]
+
+            class << self
+                attr_accessor :prevWorkName, :prevWorkPostName, :prevWorkSalary
+            end
+
+            prevWorkName = args[10]
+			prevWorkPostName = args[11]
+            prevWorkSalary = args[12]
+
+        else
+            raise "Количество аргументов должно равняться 9 или 13"
+        end
+    end
+end
+
+
+ivan = Employee.new("Иванов", "Иван", "Иванович", "03.04.1988", "+79957517964",
+                      "myMail@mail.ru", "0315 224124", "Программист", "Красная 12")
+ 
+fedor = Employee.new("Федоров", "Федор", "Федорович", "03.04.1986", "+79957517912",
+                      "fedor@mail.ru", "0315 224124", "Инженер", "Ставропольская 15", 10, "KUBGU", "Старший преподаватель", 40000)
+ 
+ 
